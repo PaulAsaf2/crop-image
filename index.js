ce7235e9d075983f1864be514a1eb7adbc5be360
Pavel Asafov <pavelaof@gmail.com>
2024-03-15 12:33:21 +0300

    add corse lib

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
