15b3366c0c502726884d5546d5ac5f6575122f28
Pavel Asafov <pavelaof@gmail.com>
2024-03-22 15:50:49 +0300

    change path to upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // const dest = `C:\\Users\\pavel\\repositories\\Telegram Mini App\\crop-image\\backend\\uploads`
-    const dest = '/root/wallstring/webapp1/uploads';
+    const dest = '/var/www/wallstring/webapp1/uploads';
     // const dest = '/root/crop-image/uploads';
     cb(null, dest);
   },
